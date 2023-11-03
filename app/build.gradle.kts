4de696ee47075492322a41e76f8cdf31af9eb199
zhihaofans <zhihaofans@hotmail.com>
2023-11-03 09:45:07 +0800

    更新依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,10 +61,16 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
@@ -75,8 +81,9 @@ dependencies {
     // Log库Logger <https://github.com/orhanobut/logger>
     implementation("com.orhanobut:logger:2.2.0")
     // https://github.com/zhihaofans/AndroidLibrary
-    debugImplementation(files("libs/android_library-debug.aar"))
-    releaseImplementation("io.zhihao:AndroidLibrary:0.0.11-jitpackfix-2")
+    debugImplementation(files("libs/android_library-release.aar"))
+    releaseImplementation("io.zhihao:AndroidLibrary:0.0.13")
+    // Gson
     implementation("com.google.code.gson:gson:2.10.1")
     // 图片加载库Coil <https://github.com/coil-kt/coil>
     implementation("io.coil-kt:coil-compose:2.4.0")
